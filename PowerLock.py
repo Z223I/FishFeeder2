d4adcb348b05693ea3089cff89fca720b3b28658
Z223I <BruceWilsonPH@yahoo.com.ph>
2017-11-25 12:51:04 -0600

    Transferred main from PowerLock to a seperate file.

--- PowerLock.py
+++ PowerLock.py
@@ -53,10 +53,10 @@ class PowerLock():
 #      GPIO.setup(pin,GPIO.OUT)
 #      GPIO.output(pin, False)
 
-    GPIO.setup(powerLockPinA, GPIO.OUT)
-    GPIO.output(powerLockPinA, False)
-    GPIO.setup(powerLockPinB, GPIO.OUT)
-    GPIO.output(powerLockPinB, False)
+    GPIO.setup(self.powerLockPinA, GPIO.OUT)
+    GPIO.output(self.powerLockPinA, False)
+    GPIO.setup(self.powerLockPinB, GPIO.OUT)
+    GPIO.output(self.powerLockPinB, False)
 
 # End Function init
 
@@ -113,7 +113,7 @@ class PowerLock():
 
 
 ########################################################
-# Function lock
+# Function unlock
 ########################################################
 
   def unlock(self):
@@ -129,50 +129,8 @@ class PowerLock():
 # End Function unlock
 ########################################################
 
-
-
-########################################################
-# Function shutdown
-########################################################
-
-
-def shutdown():
-  GPIO.cleanup()
-  print
-  print "Bye!"  
-
-# End Function shutdown
-
-
-
 ########################################################
 #
-# Main
-#
-########################################################
-
-try:
-  powerLockPinA = 17
-  powerLockPinB = 18
-
-  powerLock = PowerLock(powerLockPinA, powerLockPinB)
-
-  powerLock.init()
-  powerLock.cycle()
-
-# End try
-
-except KeyboardInterrupt:
-  # This statement is meaningless other than it allows the program to
-  # drop down to the next line.
-  print "Keyboard Interrupt"
-
-# End except
-
-shutdown()
-
-########################################################
-#
-# End Main
+# End class PowerLock
 #
 ########################################################
